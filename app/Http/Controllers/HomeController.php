f3729cf624df13a270443005f5dc4e244e5cd9d3
Naru0003 <nrm.xxx@docomo.ne.jp>
2023-10-19 20:39:40 +0900

    SimpleMemo メモ機能の開発

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Memo;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $posts = $request->all();
+        // dd(\Auth::id());
+
+        Memo::insert(['content' => $posts['content'],'user_id' => \Auth::id()]);
+        
+        return redirect( route('home'));
+    }
+}
